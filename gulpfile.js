be95af154f80bffd44f9e2643e550fa3cd7bdd21
Ryo Taga <taga@n-di.co.jp>
2017-06-26 01:17:03 +0900

    update the env for ejs

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp'),
+      fs = require('fs'),
       $ = require('gulp-load-plugins')();
 
 const Path = {
   'Scss': 'src/scss/',
+  'Ejs': 'src/ejs/',
   'Dist': 'dist/',
 };
 
@@ -21,10 +23,43 @@ gulp.task('scss', () => {
         .pipe(gulp.dest(`${Path.Dist}css/`))
 });
 
+gulp.task('ejs', () => {
+  const config = JSON.parse(fs.readFileSync(`${Path.Ejs}config.json`));
+  const data   = config.data;
+  const pages  = config.pages;
+    for (const page of pages) {
+        gulp.src(`${Path.Ejs}**.ejs`)
+            .pipe($.ejs({
+                data: data,
+                page: page
+            }))
+            .pipe($.rename(`${page.filename}.html`))
+            .pipe(gulp.dest(`${Path.Dist}${page.directory}`));
+    }
+});
+
+
+// gulp.task('ejs', () => {
+//   const config = JSON.parse(fs.readFileSync(`${Path.Ejs}config.json`));
+//   const data   = config.data;
+//   const pages  = config.pages;
+//   for (const page of pages) {
+//     gulp.src(`${Path.Ejs}**/**.ejs`)
+//     .pipe($.ejs({
+//       data: data,
+//       page: page
+//     }))
+//     .pipe($.rename(`${page.filename}.html`))
+//     .pipe(gulp.dest(`${Path.Dist}${page.directory}`));
+//   }
+// });
+
 const tasks = [
   'scss',
+  'ejs',
 ];
 
 gulp.task('default', tasks, () => {
   gulp.watch([`${Path.Scss}**/*.scss`], ['scss']);
+  gulp.watch([`${Path.Ejs}**/**.ejs`], ['ejs']);
 });

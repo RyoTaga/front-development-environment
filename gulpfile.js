b50ade62ed7ea1180ea5b1c0fddf5ab21577bc26
Ryo Taga <taga@n-di.co.jp>
2017-07-08 11:09:41 +0900

    update the env for images

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const gulp = require('gulp'),
 const Path = {
   'Scss': 'src/scss/',
   'Ejs': 'src/ejs/',
+  'Img': 'src/img/',
   'Dist': 'dist/',
 };
 
@@ -38,28 +39,25 @@ gulp.task('ejs', () => {
     }
 });
 
-
-// gulp.task('ejs', () => {
-//   const config = JSON.parse(fs.readFileSync(`${Path.Ejs}config.json`));
-//   const data   = config.data;
-//   const pages  = config.pages;
-//   for (const page of pages) {
-//     gulp.src(`${Path.Ejs}**/**.ejs`)
-//     .pipe($.ejs({
-//       data: data,
-//       page: page
-//     }))
-//     .pipe($.rename(`${page.filename}.html`))
-//     .pipe(gulp.dest(`${Path.Dist}${page.directory}`));
-//   }
-// });
+gulp.task('img', () => {
+    gulp.src(`${Path.Img}*`)
+        .pipe($.imagemin([
+            $.imagemin.gifsicle({interlaced: true}),
+            $.imagemin.jpegtran({progressive: true}),
+            $.imagemin.optipng({optimizationLevel: 7}),
+            $.imagemin.svgo({plugins: [{removeViewBox: true}]})
+        ]))
+        .pipe(gulp.dest(`${Path.Dist}img/`));
+});
 
 const tasks = [
   'scss',
   'ejs',
+  'img',
 ];
 
 gulp.task('default', tasks, () => {
   gulp.watch([`${Path.Scss}**/*.scss`], ['scss']);
   gulp.watch([`${Path.Ejs}**/**.ejs`], ['ejs']);
+  gulp.watch([`${Path.Img}*`], ['img']);
 });
